07d8825628409ea70025545858d2e2fd8b4d15c9
= <=>
2019-05-07 14:50:51 +0200

    port conflict resolve

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import app from './App'
 require('dotenv').config()
 const {exec} = require('child_process')
-const port = process.env.PORT || 4001
-app.engine('html', require('ejs').renderFile);
+let {nextAvailable} = require('node-port-check')
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-  return console.log(`NodeJS server listening at port ${port}`)
-})
+const server = async () => {
+  let port = await nextAvailable(4001, '0.0.0.0')
+  app.engine('html', require('ejs').renderFile);
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log(err)
+    }
+    return console.log(`NodeJS server listening at port ${port}`)
+  })
+}
+
+server()
